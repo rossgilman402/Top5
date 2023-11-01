ddfb9255c1390ad7e03add237b74c13e0fdf6e18
Guadeloupe33 <stevenfrancius@gmail.com>
2023-11-01 11:27:32 -0400

    added passport

--- server/config/passport-local-strategy.js
+++ server/config/passport-local-strategy.js
@@ -1,3 +1,22 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/user");
+
+passport.use(
+	new LocalStrategy(
+		{ usernameField: "email", passReqToCallback: true },
+		async (req, email, password, done) => {
+			try {
+				let user = await User.findOne({ email: email });
+				if (!user || user.password !== password) {
+					req.flash("error", "User not Found !!!");
+					return done(null, false);
+				}
+				return done(null, user);
+			} catch (error) {
+				req.flash("error", "Error in finding the User !!!");
+				return done(error);
+			}
+		}
+	)
+);
