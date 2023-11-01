613c1db2960852e1062236ceb78f7d391505de51
Guadeloupe33 <stevenfrancius@gmail.com>
2023-11-01 12:34:45 -0400

    added passport to the server

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ const startApolloServer = async () => {
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  app.use(passport.initialize());
+ 
 
   app.use(
     '/graphql',
