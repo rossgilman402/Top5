e5a085a3f1c2b3908ff9a017242bf77b8adac25e
mhorton <horton-michael@users.noreply.github.com>
2023-11-01 11:16:26 -0400

    fixes graphql connection with user model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,13 @@
 const typeDefs = `
     type User {
         _id: ID 
-        email: !String
-        password: !String
+        email: String!
+        password: String!
     }
 
     type Query {
         getUsers: [User]
-        getUser(_id: ID!): User
+        getSingleUser(userId: ID!): User
     }
 
     type Mutation {
