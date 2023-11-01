8c1ef47f1c2f6d314d2ac9b7fc9095577a8bce9b
Guadeloupe33 <stevenfrancius@gmail.com>
2023-11-01 12:33:47 -0400

    added passport to server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const passportLocal = require("./config/passport-local-strategy");
-
+const passportSpotify = require("./config/passport-spotify-strategy");
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
