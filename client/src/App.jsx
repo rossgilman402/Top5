5beb26a6585f6199c8bb377fae95e6f6742276ca
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-02 13:07:57 -0400

    adds routes to main and fixes app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-// const Login = require('../src/pages/Login');
-import Login from "./pages/Login";
 import Navbar from "./components/Navbar/Navbar";
-import PlayList from "./components/PlayList";
 
 import {
   ApolloClient,
@@ -37,12 +34,7 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Navbar />
         <Outlet />
-        <PlayList />
-        <h1>Welcome to Top5</h1>
-        <Outlet />
-        {Auth.loggedIn() && <h2>Logged In!</h2>}
       </ApolloProvider>
     </>
   );
