0d6413e3d7e01d54f7d3d73d4fc04688ff9dc966
mhorton <horton-michael@users.noreply.github.com>
2023-11-02 13:16:31 -0400

    fixes bcrypt import

--- server/config/passport-local-strategy.js
+++ server/config/passport-local-strategy.js
@@ -1,61 +1,60 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
-require('dotenv').config ();
-
+require('dotenv').config();
 
 passport.use(
-    new LocalStrategy(
-        { usernameField: "email" },
-        async (email, password, done) => {
-            try {
-                let user = await User.findOne({ email: email });
-                if (!user) {
-                    return done(null, false, { message: "Incorrect email." });
-                }
-
-                // Compare hashed password
-                const isMatch = await bcrypt.compare(password, user.password);
-                if (!isMatch) {
-                    return done(null, false, { message: "Incorrect password." });
-                }
-
-                return done(null, user);
-            } catch (error) {
-                return done(error);
-            }
+  new LocalStrategy(
+    { usernameField: 'email' },
+    async (email, password, done) => {
+      try {
+        let user = await User.findOne({ email: email });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email.' });
         }
-    )
+
+        // Compare hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
 );
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error, null);
-    }
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 });
 
 passport.checkAuthentication = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    // Redirect or handle the response for unauthenticated requests
-    return res.redirect("/users/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // Redirect or handle the response for unauthenticated requests
+  return res.redirect('/users/login');
 };
 
 passport.setAuthenticatedUser = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        res.locals.user = req.user;
-    }
-    next();
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+  }
+  next();
 };
 
 module.exports = passport;
