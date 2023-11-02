633f8703284e95a20beb76fb6b846c34e3ac7f79
mhorton <horton-michael@users.noreply.github.com>
2023-11-02 13:13:20 -0400

    adds PlayList component to Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Navbar from '../components/Navbar/Navbar';
-import PlayList from '../components/PlayList/index.js';
+import PlayList from '../components/PlayList';
 const Home = () => {
   return (
     <div>
       <Navbar />
       Home
+      <PlayList />
     </div>
   );
 };
