23c1cc8a8e4a83b939e9bfc91d52446c88c58fec
Guadeloupe33 <stevenfrancius@gmail.com>
2023-11-02 10:23:51 -0400

    added .env file

--- server/config/passport-spotify-strategy.js
+++ server/config/passport-spotify-strategy.js
@@ -1,43 +1,39 @@
 const passport = require("passport");
-const spotifyStrategy = require("passport-spotify").Strategy;
-//const crypto = require("crypto");
+const SpotifyStrategy = require("passport-spotify").Strategy;
+const crypto = require("crypto");
 const User = require("../models/User");
+const env = require('./path_to_your_env_file'); // Import your environment variables
 
-// const SpotifyWebAPI = require("spotify-web-api-node");
 passport.use(
-	new spotifyStrategy(
-		{
-			clientID: env.spotify_client_id,
-			clientSecret: env.spotify_client_secret,
-			callbackURL: env.spotify_callback_url,
-			passReqToCallback: true,
-		},
-		async (req, accessToken, refreshToken, expires_in, profile, done) => {
-			try {
-				let user = await User.findOne({ email: profile.emails[0].value });
+    new SpotifyStrategy(
+        {
+            clientID: env.spotify_client_id,
+            clientSecret: env.spotify_client_secret,
+            callbackURL: env.spotify_callback_url,
+            passReqToCallback: true
+        },
+        async (req, accessToken, refreshToken, expires_in, profile, done) => {
+            try {
+                let user = await User.findOne({ email: profile.emails[0].value });
 
-				// const playLists = await getSpotifyData(accessToken, refreshToken);
-				// req.body.playlists = playLists;
+                if (user) {
+                    return done(null, user);
+                }
 
-				if (user) {
-					return done(null, user);
-				}
-				user = await User.create({
-					//name: profile.displayName,
-					email: profile.emails[0].value,
-					password: crypto.randomBytes(20).toString("hex"),
-					//avatar: profile?.photos[0]?.value
-						//? profile.photos[0].value
-						//: "https://avatars.githubusercontent.com/u/142236345?s=400&v=4",
-				});
-				return done(null, user);
-			} catch (error) {
-				console.log(error);
-				req.flash("error", "Error in finding/creating the User !!!");
-				return done(error);
-			}
-		}
-	)
+                user = await User.create({
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString("hex"),
+                    // Add other user fields as necessary
+                });
+
+                return done(null, user);
+            } catch (error) {
+                console.error(error);
+                // Handle the error appropriately
+                return done(error);
+            }
+        }
+    )
 );
 
 module.exports = passport;
