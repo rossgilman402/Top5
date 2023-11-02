e212af61d6e91c88a81a903037c0a80f8cbec6c0
ross402 <65184393+ross402@users.noreply.github.com>
2023-11-02 17:38:48 -0400

    fixed the style and structure for the login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,47 +1,8 @@
-// import "./Login.css";
-// import { useState } from "react";
-// import SpotifyLogo from "../../assets/Spotify-logo.png";
-// import { Link } from "react-router-dom";
+import "./Login.css";
+import Top5 from "../../assets/top5-logo.png";
+import SpotifyLogo from "../../assets/spotify-logo.png";
+import SoundCloudLogo from "../../assets/soundcloud.png";
 
-// const Login = () => {
-//   const [accessToken, setAccessToken] = useState("");
-//   //Where to store accessToken?
-//   //In local storage?
-
-//   const submitSpotifty = async () => {
-//     //Call our route that will post and get access to spotify token
-//     //Then if success redirect to homepage if not then send back to login page
-//     const response = await fetch("/getAccessToken", {
-//       method: "POST",
-//     });
-//     if (response.ok) {
-//       const data = await response.json();
-//       setAccessToken(data.access_token);
-//       return data;
-//     } else {
-//       throw new Error("Failed to get access token in React");
-//     }
-//   };
-
-//   return (
-//     <div className="main-container">
-//       <div className="login-container">
-//         <h1>Login or Create Account</h1>
-//         <h3>Via Spotify</h3>
-//         <img src={SpotifyLogo} alt="Spotify Logo"></img>
-//         <button onClick={() => submitSpotifty}>Login</button>
-//         <p>
-//           Don&apos;t have an account? <a href="#">Sign up for Spotify</a>
-//         </p>
-//         <Link className="goback" to="/">
-//           Go Back
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
@@ -86,53 +47,57 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{" "}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: "pointer" }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+    <div className="main-container">
+      {data ? (
+        <Link to="/"></Link>
+      ) : (
+        <div className="login-container">
+          <h1>Login or Create Account</h1>
+          <img src={Top5} alt="Top5 Logo"></img>
+          <form onSubmit={handleFormSubmit}>
+            <label htmlFor="email">Email:</label>
+            <input
+              className="form-input"
+              placeholder="Email"
+              name="email"
+              type="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              className="form-input"
+              placeholder="**********"
+              name="password"
+              type="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+            <button className="btn" style={{ cursor: "pointer" }} type="submit">
+              Submit
+            </button>
+          </form>
+          {error && (
+            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+          )}
+          <p>
+            Don&apos;t have an account? <a href="#">Sign up for Top5</a>
+          </p>
+          <p>Or sign in with</p>
+          <div className="extra-signup">
+            <button>
+              <img src={SpotifyLogo} alt="Spotify Logo"></img>
+            </button>
+            <button>
+              <img src={SoundCloudLogo} alt="Sound Cloud Logo"></img>
+            </button>
           </div>
+          <Link className="goback" to="/">
+            Go Back
+          </Link>
         </div>
-      </div>
-    </main>
+      )}
+    </div>
   );
 };
 
