285bb0444e0bc542dc7f0757302c26e239d0ffd7
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-02 14:16:09 -0400

    adds added song component to make playlist page

--- client/src/pages/MakePlaylist.jsx
+++ client/src/pages/MakePlaylist.jsx
@@ -1,9 +1,12 @@
+import AddedSong from "../components/AddedSong/AddedSong";
 import PlaylistNameImage from "../components/PlaylistNameImage/PlaylistNameImage";
+import AddedSong from "../components/AddedSong/AddedSong";
 
 const MakePlaylist = () => {
   return (
     <div>
       <PlaylistNameImage />
+      <AddedSong />
     </div>
   );
 };
