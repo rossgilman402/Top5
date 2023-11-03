ddaa6fabe80826cdd67a32ab56f15310f08a1ecb
mhorton <horton-michael@users.noreply.github.com>
2023-11-03 15:40:08 -0400

    adds artist name to song list component

--- client/src/components/AddedSong/AddedSong.jsx
+++ client/src/components/AddedSong/AddedSong.jsx
@@ -5,7 +5,7 @@ const AddedSong = ({ song, onRemove }) => {
   return (
     <div className="added-song">
       <p className="song-name">{song.name}</p>
-      {/* <p>Artist: {song.artist}</p> */}
+      <p>Artist: {song.artists[0].name}</p>
       {/* <p>Album: {song.album}</p> */}
       <button className="remove-btn" onClick={onRemove}>
         Remove
