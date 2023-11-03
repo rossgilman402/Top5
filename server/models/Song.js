0ebc10db02e6beea4cd888dcb6881a3ce0ad068e
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-03 10:34:21 -0400

    adds song model

---
+++ server/models/Song.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  artist: { type: String, required: true },
+  song: { type: String, required: true },
+  img: { type: String, required: true },
+  duration: { type: String, required: true },
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+module.exports = Song;
