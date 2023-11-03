f17b09c33968915e636689a16250454487202cf5
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-03 10:39:11 -0400

    adds playlist to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   email: {
@@ -13,10 +13,11 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
+  playlists: { type: [String], default: [] },
 });
 // Set up pre-save middleware to create password
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -26,6 +27,6 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
