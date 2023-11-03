cc0c230ba29b615d3ab67a63385b86fc97b140d6
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-03 10:37:54 -0400

    adds playlist model

---
+++ server/models/Playlist.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const playListSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  user: { type: ObjectId, ref: "user", required: true },
+  desc: { type: String },
+  songs: { type: Array, default: [] },
+  img: { type: String },
+});
+
+const PlayList = mongoose.model("playList", playListSchema);
+
+module.exports = { PlayList, validate };
--- server/models/Song.js
+++ server/models/Song.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const songSchema = new mongoose.Schema({
   name: { type: String, required: true },
