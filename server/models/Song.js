2eb4e89876ccc2663d64025740141d5f9a8eb118
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-03 10:43:13 -0400

    adds uri to songs

--- server/models/Song.js
+++ server/models/Song.js
@@ -6,6 +6,7 @@ const songSchema = new mongoose.Schema({
   song: { type: String, required: true },
   img: { type: String, required: true },
   duration: { type: String, required: true },
+  uri: { type: String, required: true },
 });
 
 const Song = mongoose.model("Song", songSchema);
