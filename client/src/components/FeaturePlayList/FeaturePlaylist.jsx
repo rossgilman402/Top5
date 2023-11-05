c391c87ceedd50b723bee22ed4992e19f5e6ddf6
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-05 12:39:40 -0500

    adds featured playlist box

--- client/src/components/FeaturePlayList/FeaturePlaylist.jsx
+++ client/src/components/FeaturePlayList/FeaturePlaylist.jsx
@@ -2,16 +2,33 @@ import { Link } from "react-router-dom";
 import "./featuredPlaylist.css";
 
 const FeaturePlaylist = () => {
+  const playlist = ["Featured Playlist of the Day"];
   return (
-    <div className="featured-container">
-      <div className="feature-playlist">
-        <Link className="link" to="/chat">
-          <div className="circle">
-            <div className="rotating-text">Featured Playlist</div>
-          </div>
-        </Link>
+    <>
+      <div className="featured-container">
+        <div className="feature-playlist">
+          <Link className="link" to="/chat">
+            <div className="circle">
+              <div className="rotating-text">Featured Playlist</div>
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
+      <div className="details">
+        <Link to={`playlist-page`}> </Link>
+        {/* <img src={song.artworkUrl} alt={song.name} /> */}
+        <img
+          className="song-art"
+          src="https://via.placeholder.com/150"
+          alt="song-art"
+        />
+        <div className="playlist-info">
+          <h4 className="playlist">{playlist}</h4>
+          <p className="artist">song.artist</p>
+          <h5 className="duration">song.duration</h5>
+        </div>
+      </div>
+    </>
   );
 };
 
