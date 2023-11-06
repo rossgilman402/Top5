73c22beffb7c10dc8ce36b717b2e9f9ce29fdabf
ross402 <65184393+ross402@users.noreply.github.com>
2023-11-05 19:48:12 -0500

    Added socket

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,12 +1,60 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
 const Chat = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  // Replace with your Socket.IO server URL
+  // const socket = io("http://localhost:3001"); // Change the URL accordingly
+
+  // useEffect(() => {
+  //   // Listen for incoming messages from the server
+  //   socket.on("chat message", (message) => {
+  //     setMessages([...messages, message]);
+  //   });
+
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, [messages, socket]);
+
+  // const sendMessage = (e) => {
+  //   e.preventDefault();
+
+  //   // Emit the message to the server
+  //   socket.emit("chat message", message);
+
+  //   // Clear the input field
+  //   setMessage("");
+  // };
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setMessage(value);
+  };
+
   return (
     <>
       <div>
         <h1>Socket.IO chat</h1>
         <div>
-          <ul id="messages"></ul>
-          <form id="form" action="">
-            <input id="input" />
+          <ul id="messages">
+            {messages.map((msg, index) => (
+              <li key={index}>{msg}</li>
+            ))}
+          </ul>
+          <form
+            // onSubmit={sendMessage}
+            id="form"
+            action=""
+          >
+            <input
+              id="input"
+              placeholder="Send a message..."
+              value={message}
+              onChange={handleChange}
+            />
             <button>Send</button>
           </form>
         </div>
--- server/server.js
+++ server/server.js
@@ -1,17 +1,123 @@
+// const express = require("express");
+// const { ApolloServer } = require("@apollo/server");
+// const { expressMiddleware } = require("@apollo/server/express4");
+// const path = require("path");
+// const { authMiddleware } = require("./utils/auth");
+// const { typeDefs, resolvers } = require("./schemas");
+// const db = require("./config/connection");
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const cookieSession = require("cookie-session");
+// const passport = require("passport");
+// const passportSetup = require("./passport");
+// const authRoute = require("./routes/auth-routes");
+// // const cors = require("cors");
+// const { createServer } = require("node:http");
+// const { Server } = require("socket.io");
+
+// //Create our server
+// const socketServer = createServer(app);
+// const io = new Server(socketServer);
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+
+// socketServer.listen(5000, () => {
+//   console.log("Socket Server running at http://localhost:5000");
+// });
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: ["passsord"],
+//     maxAge: 24 * 60 * 60 * 100,
+//   })
+// );
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// // app.use(
+// //   cors({
+// //     origin: "http://localhost:3001",
+// //     methods: "GET, POST, PUT, DELETE",
+// //     credentials: true,
+// //   })
+// // );
+// app.use("/auth", authRoute);
+
+// // Create a new instance of an Apollo server with the GraphQL schema
+// const startApolloServer = async () => {
+//   await server.start();
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// app.use(
+//   "/graphql",
+//   expressMiddleware(server, {
+//     context: authMiddleware,
+//   })
+// );
+
+//   if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../client/dist")));
+
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+//     });
+//   }
+
+//   db.once("open", () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// // Call the async function to start the server
+// startApolloServer();
 const express = require("express");
-const { ApolloServer } = require("@apollo/server");
+const { ApolloServer } = require("apollo-server-express");
 const { expressMiddleware } = require("@apollo/server/express4");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const path = require("path");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-const passportSetup = require("./passport");
 const authRoute = require("./routes/auth-routes");
+// const { Server } = require("socket.io");
+const { authMiddleware } = require("./utils/auth");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const http = require("http").Server(app);
 const cors = require("cors");
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+io.attach(http); // Attach the WebSocket server to the HTTP server
 
 const server = new ApolloServer({
   typeDefs,
@@ -28,28 +134,21 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3001",
-//     methods: "GET, POST, PUT, DELETE",
-//     credentials: true,
-//   })
-// );
-app.use("/auth", authRoute);
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
+const startServers = async () => {
   await server.start();
-
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-
   app.use(
     "/graphql",
     expressMiddleware(server, {
       context: authMiddleware,
     })
   );
+
+  app.use("/auth", authRoute);
+
+  server.applyMiddleware({ app });
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
@@ -60,12 +159,14 @@ const startApolloServer = async () => {
   }
 
   db.once("open", () => {
-    app.listen(PORT, () => {
+    http.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+      console.log(`Socket Server running at http://localhost:${PORT}`);
     });
   });
 };
 
-// Call the async function to start the server
-startApolloServer();
+startServers();
