bc2bf70f104e3d91779d3788a16a6b6d9daecd21
mhorton <horton-michael@users.noreply.github.com>
2023-11-06 21:25:57 -0500

    sets Featured Playlist in localstorage for current day, checks if Featured Playlist exists in localStorage and sets random Playlist to Featured if not

--- client/src/components/FeaturePlayList/FeaturePlaylist.jsx
+++ client/src/components/FeaturePlayList/FeaturePlaylist.jsx
@@ -1,15 +1,35 @@
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_FEATURED_PLAYLIST } from '../../utils/query';
+import { useState, useEffect } from 'react';
 import './featuredPlaylist.css';
 
 const FeaturePlaylist = () => {
-  const { loading, error, data } = useQuery(GET_FEATURED_PLAYLIST);
+  const [playlist, setPlaylist] = useState(
+    JSON.parse(localStorage.getItem('featuredPlaylist'))
+  );
+  const date = new Date().toDateString();
+
+  const { loading, error, data } = useQuery(GET_FEATURED_PLAYLIST, {
+    skip: playlist && localStorage.getItem('featuredPlaylistDate') === date,
+  });
+
+  useEffect(() => {
+    if (data && data.featuredPlaylist) {
+      setPlaylist(data.featuredPlaylist);
+      localStorage.setItem(
+        'featuredPlaylist',
+        JSON.stringify(data.featuredPlaylist)
+      );
+      localStorage.setItem('featuredPlaylistDate', date);
+    }
+  }, [data, date]);
+
   console.log('DATAAA', data);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error!</div>;
 
-  const playlist = data.featuredPlaylist;
+  // const playlist = data.featuredPlaylist;
 
   // const playlist = ['Featured Playlist of the Day'];
   return (
@@ -27,21 +47,26 @@ const FeaturePlaylist = () => {
           </Link>
         </div>
       </div>
-      <Link to="/Playlist" style={{ 'text-decoration': 'none', width: '50%' }}>
-        <div className="playlist-details">
-          {/* <img src={song.artworkUrl} alt={song.name} /> */}
-          <img
-            className="playlist-art"
-            src="https://via.placeholder.com/150"
-            alt="song-art"
-          />
-          <div className="playlist-info">
-            <h4 className="playlist-title">{playlist.name}</h4>
-            <p className="user">{playlist.user.email}</p>
-            {/* <h5 className="duration">playlist.length</h5> */}
+      {playlist && (
+        <Link
+          to="/Playlist"
+          style={{ 'text-decoration': 'none', width: '50%' }}
+        >
+          <div className="playlist-details">
+            {/* <img src={song.artworkUrl} alt={song.name} /> */}
+            <img
+              className="playlist-art"
+              src={playlist.songs[0].img}
+              alt="song-art"
+            />
+            <div className="playlist-info">
+              <h4 className="playlist-title">{playlist.name}</h4>
+              <p className="user">{playlist.user.email.split('@')[0]}</p>
+              {/* <h5 className="duration">playlist.length</h5> */}
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      )}
     </>
   );
 };
